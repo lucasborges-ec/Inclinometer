076c8cc5325d5bfcb023ec9dc461158242d980c4
lucasborges-ec <lucasborges.ec@gmail.com>
2021-01-13 23:25:45 +0000

    .

--- inclinometer.py
+++ inclinometer.py
@@ -1,26 +1,22 @@
 import spidev
 import time
-import datetime
-import os
-import numpy as np
 import serial
 import struct
-
-
+time.sleep(10) #Wainting for the end of booting time
 #-----------------------------------------------------------------------------------#
 '''
 Recebe infomações preliminares como 'porta' e 'baudrate' a fim de dar início à
 comunicação serial
 '''
 def intConfig(port, baudRate):
-    global ser                                       
+    global ser
     ser = serial.Serial(port, baudRate, timeout=1)
-    ser.stopbits = serial.STOPBITS_ONE            # Dois bits como final de cada byte (representou uma melhor comportamento)
-    ser.bytesize = 8                              # Tamanho do byte   
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.bytesize = 8                              # Tamanho do byte
     ser.parity = serial.PARITY_NONE               # Não há verificação de paridade
-    
+
     print ("Serial port opened " + port + " over Baudrate " + str(baudRate))
-    
+
     return ser
 #-----------------------------------------------------------------------------------#
 
@@ -40,16 +36,17 @@ spi2.mode = 0b00
 
 
 #Starts serial communication
-port=['/dev/ttyS0']
+port=['/dev/ttyS0','/dev/ttyAMA0']
 baudRate=9600
 
-ser=intConfig(port[0], baudRate)
+ser=intConfig(port[1], baudRate)
 
 # READING SENSORS
 
 try:
     while True:
         # Sensor 1
+        test=spi1.xfer([0b00000000, 0x0,0x0])
         d1a=spi1.xfer([0b00010000, 0x0, 0x0])        # Sending 2 additional bytes (0x0) in order to have enough time to receave 11 bits from the sensor
         d1b=spi1.xfer([0b00010001, 0x0, 0x0])        # Try xfer2!!!
         x1=( d1a[1] << 8 ) + d1a[2] >> 5                                           # 11 bits in 2 bytes size mensage
@@ -61,21 +58,15 @@ try:
         d2b=spi2.xfer([0b00010001, 0x0, 0x0])
         x2=( d2a[1] << 8 ) + d2a[2] >> 5
         y2=( d2b[1] << 8 ) + d2b[2] >> 5
-        a2=(x2-y2)/6554.0
+       	a2=(x2-y2)/6554.0
 
-   
-        s1=np.rad2deg(np.arcsin(a1))
-        s2=np.rad2deg(np.arcsin(a2))
-        print("SENSOR 1 \t         SENSOR 2")
-        print(s1,"° \t",s2,"°")
-        
-        data1=struct.pack('d',s1)
-        data2=struct.pack('d',s2)
-        data=[data1,data2]
-        ser.write('<<'.encode())
+        data1=struct.pack('d',a1)
+        data2=struct.pack('d',a2)
+        ser.write('<'.encode())
+        ser.write('<'.encode())
         ser.write(data1)
         ser.write(data2)
-        
+
         time.sleep(1)
 except KeyboardInterrupt:
     spi1.close()
